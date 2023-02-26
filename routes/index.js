29ab8391e913b1aab7315d10d24a4c95d03a75ec
Colleen Que <quecolleen@gmail.com>
2023-02-26 10:42:06 -0500

    added clothing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const homeRoute = require('./home');
+const clothingRoute = require('./clothing');
+
 const constructorMethod = (app) => {
   app.use('/home', homeRoute);
-  
+  app.use('/clothing', clothingRoute);
+
   app.use('*', (req, res) => {
     res.redirect('/home');
   });
