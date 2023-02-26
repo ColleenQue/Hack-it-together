df95508cdf60ccbd5744cebec65f9a5a9049b270
AthenaKiriakoulis <athena.kiriakoulis@gmail.com>
2023-02-26 17:44:28 -0500

    fixed it

--- app.js
+++ app.js
@@ -25,9 +25,6 @@ app.listen(PORT, () => {
   console.log('Your routes will be running on http://localhost:3000');
 });
 
-app.listen(3000, function(){
-  console.log('Your routes will be running on http://localhost:3000');
-});
 
 
 
