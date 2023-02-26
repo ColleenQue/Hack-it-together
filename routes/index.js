51537f408388f50192e9e9f3a49a43b81f25b57d
Colleen Que <quecolleen@gmail.com>
2023-02-26 11:44:09 -0500

    added transport page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const homeRoute = require('./home');
 const clothingRoute = require('./clothing');
+const transportationRoute = require('./transportation');
 
 const constructorMethod = (app) => {
   app.use('/home', homeRoute);
   app.use('/clothing', clothingRoute);
-
+  app.use('/transportation', transportationRoute);
   app.use('*', (req, res) => {
     res.redirect('/home');
   });
